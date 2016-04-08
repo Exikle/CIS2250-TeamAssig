3611343e62ec0534ed279461d2779e55d10a79d4
Greewn <hnguye08@mail.uoguelph.ca>
2016-04-08 18:05:39 -0400

    merge

--- cis2250project.pl
+++ cis2250project.pl
@@ -1039,13 +1039,15 @@ sub valGet{
     my $total = 0;
     my $continue = 0;
     my $valid = 0;
+    my $temp;
 
     my $userInput;
 
-    #clearScreen();
+    clearScreen();
 
     print "Please select the field(s)[All fields are in user manual]".$NEW_LINE;
-    print "The # of [Field One] with [Field Two] with... (no limit to # of fields)".$NEW_LINE.$NEW_LINE;
+    print "The # of [Field One] with [Field Two] with... (no limit to # of fields)".$NEW_LINE;
+    print "Can not use Fields of different type (ie: birth with deaths)".$NEW_LINE.$NEW_LINE;
     while ($continue == 0) {
         $valid = 0;
         print "Field: ";
@@ -1060,16 +1062,23 @@ sub valGet{
                     print "You've already used ".$userInput.$NEW_LINE.$NEW_LINE;
                     last;
                 }
+                if (substr($userInput, 0,1) ne substr($fields[$k], 0, 1)) {
+                    clearScreen
+                    $valid = 1;
+                    print "Not the same type as initial Field".$NEW_LINE.$NEW_LINE;
+                    last;
+                }
+                
 
             }
 
             if ($valid == 0) {
                 $fields[$fieldCount] = $userInput;
                 $fieldCount++;
-                print "Add new field? (y)es or (n)o.".$NEW_LINE;
+                print "Add new field? (y)es or (n)o: ";
                 $userInput = readInput();
+                print $NEW_LINE;
                 if(lc($userInput) eq "n") {
-                    print "Triggered".$NEW_LINE;
                     $continue = 1;
                 }
             }   
@@ -1086,7 +1095,7 @@ sub valGet{
         $total += openFile($years[0]+$i, @fields, $fieldCount);
     }
 
-    #clearScreen();
+    clearScreen();
 
     print "The number of ".$fields[0];
 
@@ -1109,11 +1118,19 @@ sub openFile{
     my $record_count = 0;
     my $total = 0;
     my $value = "";
-    my $element = "";\
+    my $element = "";
+    my $count = 0;
+    my $fileName;
 
-    my $year_fh;
-    my $type;
-    my $fileName = "Data/".$type."/".lc($type).$year.".txt";
+    if (substr($fields[0], 0, 1) eq "D") {
+        $fileName = "Data/Death/deaths".$year.".txt";
+    }
+
+    else {
+        $fileName = "Data/Birth/birth".$year.".txt";
+    }
+
+    
 
     open my $year_fh, '<', $fileName
        or die "Unable to open file\n";
@@ -1127,12 +1144,12 @@ sub openFile{
        if ( $csv->parse($year_record) ) {
           my @master_fields = $csv->fields();
           $record_count++;
+          $count = 0;
 
-            for my $i ( 0..$fieldCount-1 ){
+            for my $i ( 0..$fieldCount - 1 ){
 
                 $element = getFieldValue($fields[$i]);
                 $value = $master_fields[getFieldLocation($fields[$i])];
-                print $element.$NEW_LINE;
 
                 if ($value eq "M") {
                     $value = "1";
@@ -1142,14 +1159,20 @@ sub openFile{
                 }
 
                 if ($value eq $element) { #### get this resolved
-                    $total++;
+                    $count++;
                 }
+            }
+
+            if ($count == $fieldCount) {
+                $total++;
             }
        }
 
        else {
           warn "Line/record could not be parsed\n";
        }
+
+       clearScreen();
 
     }
 
